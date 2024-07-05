ae619d9cc90d8062efc9a080779c50b0c2eb59c9
Kacper Jarocki <Dev.KacperJarocki@proton.me>
2024-07-05 14:03:58 +0200

    prints networks when button is pushed

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,86 @@
 #![no_std]
 #![no_main]
 
+use core::str;
+use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
-use embassy_time::Timer;
+use embassy_net::Stack;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::{bind_interrupts, gpio};
 use gpio::{Input, Level, Output, Pull};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
-/// It requires an external signal to be manually triggered on PIN 16. For
-/// example, this could be accomplished using an external power source with a
-/// button so that it is possible to toggle the signal from low to high.
-///
-/// This example will begin with turning on the LED on the board and wait for a
-/// high signal on PIN 16. Once the high event/signal occurs the program will
-/// continue and turn off the LED, and then wait for 2 seconds before completing
-/// the loop and starting over again.
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<cyw43::NetDriver<'static>>) -> ! {
+    stack.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_17, Level::High);
-    let mut async_input = Input::new(p.PIN_16, Pull::Up);
+    let led_button = Input::new(p.PIN_16, Pull::Up);
+    spawner.spawn(button_task(led_button, led)).unwrap();
+    let network_button = Input::new(p.PIN_18, Pull::Up);
+    let fw = include_bytes!("../../../embassy/cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../../../embassy/cyw43-firmware/43439A0_clm.bin");
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
 
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    unwrap!(spawner.spawn(print_networks_task(network_button, control)));
+}
+
+#[embassy_executor::task]
+async fn button_task(mut button: Input<'static>, mut led: Output<'static>) {
     loop {
-        info!("wait_for_low . Turn off LED");
-        led.set_low();
-        info!("wating for low");
-        async_input.wait_for_low().await;
-
-        info!("done wait_for_high. Turn on LED");
-        led.set_high();
-
-        Timer::after_secs(2).await;
+        button.wait_for_falling_edge().await;
+        led.toggle();
+        info!("i am in another task");
+    }
+}
+#[embassy_executor::task]
+async fn print_networks_task(mut button: Input<'static>, mut control: cyw43::Control<'static>) {
+    loop {
+        button.wait_for_falling_edge().await;
+        info!("print networks");
+        let mut scanner = control.scan(Default::default()).await;
+        while let Some(bss) = scanner.next().await {
+            if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
+                info!("scanned {} == {:x}", ssid_str, bss.bssid);
+            }
+        }
     }
 }
