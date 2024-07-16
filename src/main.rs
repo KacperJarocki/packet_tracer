5fe22fe132d121d50e40b2925191cbadd5835d4b
KacperJarocki <dev.kacperjarocki@proton.me>
2024-07-16 11:13:34 +0200

    Added vector to send many networks

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@
 #![no_main]
 
 use core::str;
+use cyw43::BssInfo;
 use cyw43_pio::PioSpi;
-use defmt::*;
+use defmt::{info, *};
 use embassy_executor::Spawner;
 use embassy_net::Stack;
 use embassy_rp::peripherals::{DMA_CH0, I2C0, PIO0, UART0};
@@ -12,7 +13,7 @@ use embassy_rp::uart::{self, Blocking};
 use embassy_rp::{bind_interrupts, gpio};
 use embassy_time::Timer;
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    mono_font::{ascii::FONT_5X7, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
     text::{Baseline, Text},
@@ -29,11 +30,12 @@ use embassy_rp::i2c::{self, Config, I2c};
 use embassy_rp::multicore::{self, spawn_core1};
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::channel::Channel;
+use heapless::Vec;
 
 static mut CORE1_STACK: multicore::Stack<4096> = multicore::Stack::new();
 static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
 static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
-static CHANNEL: Channel<CriticalSectionRawMutex, cyw43::BssInfo, 1> = Channel::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, Vec<cyw43::BssInfo, 10>, 1> = Channel::new();
 
 #[embassy_executor::task]
 async fn wifi_task(
@@ -75,7 +77,7 @@ async fn main(spawner: Spawner) {
         uart::Uart::new_with_rtscts_blocking(p.UART0, p.PIN_0, p.PIN_1, p.PIN_3, p.PIN_2, config);
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
-    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    let (_net_device, control, runner) = cyw43::new(state, pwr, spi, fw).await;
     spawn_core1(
         p.CORE1,
         unsafe { &mut *core::ptr::addr_of_mut!(CORE1_STACK) },
@@ -84,7 +86,7 @@ async fn main(spawner: Spawner) {
             info!("set up i2c ");
             let sda = p.PIN_20;
             let scl = p.PIN_21;
-            let mut i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, Config::default());
+            let i2c = i2c::I2c::new_blocking(p.I2C0, scl, sda, Config::default());
             let interface = I2CDisplayInterface::new(i2c);
             let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
                 .into_buffered_graphics_mode();
@@ -115,7 +117,7 @@ async fn main(spawner: Spawner) {
     executor0.run(|spawner| {
         info!("spawning tasks");
         unwrap!(spawner.spawn(wifi_task(runner)));
-        unwrap!(spawner.spawn(scan_networks_task(control, uart, clm)));
+        unwrap!(spawner.spawn(scan_networks_task(control, uart, clm, network_button)));
     });
 }
 #[embassy_executor::task]
@@ -128,7 +130,7 @@ async fn change_display_output(
     mess: &'static str,
 ) {
     loop {
-        Timer::after_millis(500).await;
+        Timer::after_millis(50).await;
         info!("clearing display");
         if display.clear(BinaryColor::Off).is_err() {
             info!("clearing failed");
@@ -136,20 +138,25 @@ async fn change_display_output(
             info!("clearing successful");
         }
         let text_style = MonoTextStyleBuilder::new()
-            .font(&FONT_6X10)
+            .font(&FONT_5X7)
             .text_color(BinaryColor::On)
             .build();
         info!("displaying mess {}", mess);
         Text::with_baseline(mess, Point::zero(), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
-        let bss = CHANNEL.receive().await;
-
-        if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
-            info!("scanned {} == {:x}", ssid_str, bss.bssid);
-            Text::with_baseline(ssid_str, Point::new(0, 10), text_style, Baseline::Top)
-                .draw(&mut display)
-                .unwrap();
+        info!("waitnig for recv");
+        let mut bss_vec = CHANNEL.receive().await;
+        info!("recv channel to display");
+        for i in 0..10 {
+            let bss = bss_vec.pop().unwrap();
+            let x = 6 * i;
+            if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
+                info!("will display {} == {:x}", ssid_str, bss.bssid);
+                Text::with_baseline(ssid_str, Point::new(0, x), text_style, Baseline::Top)
+                    .draw(&mut display)
+                    .unwrap();
+            }
         }
 
         if display.flush().is_err() {
@@ -174,6 +181,7 @@ async fn scan_networks_task(
     mut control: cyw43::Control<'static>,
     mut uart: uart::Uart<'static, UART0, Blocking>,
     clm: &'static [u8],
+    mut network_button: Input<'static>,
 ) {
     info!("waitnig for load");
     control.init(clm).await;
@@ -182,15 +190,20 @@ async fn scan_networks_task(
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
     loop {
-        info!("waiting for 2 seconds");
-        Timer::after_secs(2).await;
+        info!("waiting for button");
+        network_button.wait_for_falling_edge().await;
+        info!("create a new vec");
+        let mut bss_vec: Vec<BssInfo, 10> = Vec::new();
         info!("print networks");
         let mut scanner = control.scan(Default::default()).await;
         info!("scanning");
         while let Some(bss) = scanner.next().await {
-            Timer::after_secs(1).await;
-            CHANNEL.send(bss).await;
-            info!("sending bss to channel");
+            info!("add BssInfo to vec");
+            if bss_vec.push(bss).is_err() {
+                info!("Vec is overflow")
+            } else {
+                info!("Added bss to Vec")
+            }
             if let Ok(ssid_str) = str::from_utf8(&bss.ssid) {
                 info!("scanned {} == {:x}", ssid_str, bss.bssid);
                 uart.blocking_write("Network: ".as_bytes()).unwrap();
@@ -200,5 +213,8 @@ async fn scan_networks_task(
                 uart.blocking_write("\n\r".as_bytes()).unwrap();
             }
         }
+
+        info!("sending bss_vec to channel");
+        CHANNEL.send(bss_vec).await;
     }
 }
